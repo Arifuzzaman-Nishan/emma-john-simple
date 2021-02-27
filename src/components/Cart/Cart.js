78da87b975e97ffae9bc3a6470b304397744f46d
Nishan <arifuzzamannishan@gmail.com>
2021-02-27 21:53:23 +0600

    done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,6 +1,7 @@
 import { faToriiGate } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 import { nativeTouchData } from 'react-dom/test-utils';
+import './Cart.css'
 
 
 
@@ -17,7 +18,7 @@ const Cart = (props) => {
     else if (total === 0) shippingCost = 0;
 
 
-    const tax = (total + shippingCost)*0.1;
+    const tax = (total + shippingCost) * 0.1;
 
     const formatNumber = (num) => {
         const precision = num.toFixed(2);
@@ -25,15 +26,20 @@ const Cart = (props) => {
     }
 
     return (
-        <div>
-            <h2>Order Summary</h2>
-            <h3>Items Ordered: {props.cart.length}</h3>
-            <div>
-                <p><small>Items: ${formatNumber(total)}</small></p>
+        <div className="cart">
+            <div className="cart-upper-part">
+                <h2>Order Summary</h2>
+                <h3>Items Ordered: {props.cart.length}</h3>
+            </div>
+            <div className="cart-lower-part">
+                <p ><small className="space">Items: ${formatNumber(total)}</small></p>
                 <p><small>Shopping & Handling: ${shippingCost} </small></p>
                 <p><small>Total before tax: {formatNumber(total + shippingCost)} </small></p>
                 <p><small>Estimated Tax: {formatNumber(tax)}</small></p>
-                <h3>Order Total: {formatNumber(total + shippingCost + tax)}</h3>
+                <h3 className = "color">Order Total: ${formatNumber(total + shippingCost + tax)}</h3>
+                <div style = {{textAlign : "center"}}>
+                <button className = "button">Review your order</button>
+                </div>
             </div>
         </div>
     );
